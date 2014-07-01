eaa1eb58a4a0a67b4a67d7a74b4cc079c8f4bdcc
Ichiro Kimura <kag.web@gmail.com>
2014-07-01 20:55:25 +0900

    deploy準備

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,6 @@
 Rails.application.configure do
+  HOSTNAME = 'kupikupidulu.com'
+  REDIS_HOST = 'localhost:6379'
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
