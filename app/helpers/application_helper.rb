8f13dcfb9f011a0ecfda78d67980dcb514a3043a
Ichiro Kimura <kag.web@gmail.com>
2014-07-13 13:10:15 +0900

    改行を記事に反映

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,9 @@
 module ApplicationHelper
+  def show_text text
+    return '' if text.blank?
+    sanitize text.gsub(/</, '&lt;').gsub(/\r\n|\n|\r/, '<br>'), tags: %w(br)
+  end
+
   def asset_url asset
     "#{request.protocol}#{request.host_with_port}#{asset_path asset}"
   end
