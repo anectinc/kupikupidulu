277595d320d61cf4a7a8a5abefec311442cabc34
TamakiMaeda <giraph.me@gmail.com>
2015-06-24 13:00:20 +0900

    unicornの微調整

--- config/unicorn/staging.rb
+++ config/unicorn/staging.rb
@@ -1,9 +1,9 @@
 # coding: utf-8
 worker_processes 8 # ワーカープロセス数
-working_directory "/usr/local/services/staging/kupikupidulu/current"
+working_directory "/usr/local/services/kupikupidulu/current"
 
-listen "/usr/local/services/staging/kupikupidulu/shared/pids/unicorn.sock"
-pid "/usr/local/services/staging/kupikupidulu/shared/pids/unicorn.pid"
+listen "/usr/local/services/kupikupidulu/shared/pids/unicorn.sock"
+pid "/usr/local/services/kupikupidulu/shared/pids/unicorn.pid"
 
 stderr_path File.expand_path('log/unicorn.log', ENV['RAILS_ROOT'])
 stdout_path File.expand_path('log/unicorn.log', ENV['RAILS_ROOT'])
